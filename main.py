2c26c2828716ae59b93cb0efb13568f05902d1ef
vedanshdwivedi <vedanshdwivedi0@gmail.com>
2022-09-12 00:48:28 +0530

    Added Transformation File for a Sample Project

--- main.py
+++ main.py
@@ -2,11 +2,9 @@ import importlib.util
 from importlib.machinery import SourceFileLoader
 
 
-project_path_dict = {
-    1: "./projects/humidity/"
-}
+project_path_dict = {1: "./projects/humidity/"}
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print("Select a Project to Run")
     print("List of Available Projects")
     print("1. Humidity Prediction using XGBoost")
@@ -14,8 +12,9 @@ if __name__ == '__main__':
     choice = 1
     if choice == 1:
         class_path = project_path_dict[choice] + "Transformation.py"
-        requirement_path = project_path_dict[choice] + "project_requirements.txt"
 
-        obj = SourceFileLoader("Transformation", class_path).load_module().Transformation()
-
-
+        obj = (
+            SourceFileLoader("Transformation", class_path)
+            .load_module()
+            .Transformation()
+        )
